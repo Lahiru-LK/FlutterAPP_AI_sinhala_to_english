6084bbe0a693896d1ac5ce1ecac5fe29de8f5c7e
Lahiru Prasad <104630433+Lahiru-LK@users.noreply.github.com>
2025-02-23 14:11:31 +0530

    Update App Name
            android:label="sinhala_to_english_app"
            android:label="AI Language Translator"

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:sinhala_to_english_app/main.dart';
+import 'package:sinhala_to_english_apps/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
